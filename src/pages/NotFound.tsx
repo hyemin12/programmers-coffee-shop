b1a159838f99e17291620a2d5b983d24d4f48ca9
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-02-05 16:29:53 +0900

    feat: 잘못된 경로 컴포넌트 생성

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,40 @@
+import styled from 'styled-components';
+import Layout from '../components/Layout';
+import { Button } from 'react-bootstrap';
+
 const NotFound = () => {
-	return <div>NotFound</div>;
+	return (
+		<Layout>
+			<NotFoundContainer>
+				<h1>Error 404!</h1>
+				<h3>페이지를 찾을 수 없습니다.</h3>
+				<p>원하시는 결과를 찾을 수 없습니다.</p>
+				<p>요청하신 페이지가 사라졌거나, 잘못된 경로입니다.</p>
+				<Button href='/' variant='success' size='lg'>
+					메인으로 돌아가기
+				</Button>
+			</NotFoundContainer>
+		</Layout>
+	);
 };
+const NotFoundContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	min-height: calc(100vh - 145px);
+	h1,
+	h3 {
+		font-weight: 800;
+		color: #1e3932;
+	}
+	p {
+		margin-bottom: 0;
+	}
+	a {
+		display: block;
+		margin-top: 1em;
+	}
+`;
 
 export default NotFound;
