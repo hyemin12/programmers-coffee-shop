a790f41948f930e319c7981c510a030687678f85
hyemin12 <66300732+hyemin12@users.noreply.github.com>
2024-02-06 12:37:52 +0900

    style: nav 스타일 변경

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,48 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 const NavComponents = () => {
+	const menu = [
+		{ path: '/coffee', name: 'MENU' },
+		{ path: '/company', name: 'starbucks' },
+		{ path: '/members', name: 'starbucks rewards' },
+	];
 	return (
-		<Navbar bg='dark' data-bs-theme='dark'>
+		<NavBarStyle data-bs-theme='dark'>
 			<Container>
 				<Navbar.Brand href='/'>스타벅스 프로그래머스</Navbar.Brand>
 				<Nav className='me-auto'>
-					<Nav.Link href='/coffee'>coffee</Nav.Link>
-					<Nav.Link href='/company'>company</Nav.Link>
-					<Nav.Link href='/members'>Members</Nav.Link>
+					{menu.map(({ path, name }: { path: string; name: string }) => (
+						<LinkStyle className={({ isActive }) => (isActive ? 'active' : '')} key={path} to={path}>
+							{name}
+						</LinkStyle>
+					))}
 				</Nav>
 			</Container>
-		</Navbar>
+		</NavBarStyle>
 	);
 };
+const NavBarStyle = styled(Navbar)`
+	background-color: #2c2a29;
+	a.navbar-brand {
+		font-size: 1.3em;
+		font-weight: 600;
+		margin-right: 2em;
+	}
+`;
+const LinkStyle = styled(NavLink)`
+	margin-right: 1em;
+	color: #999;
+	font-size: 0.96em;
+	font-weight: normal;
+	text-transform: uppercase;
+	transition: all 0.3s;
+	&.active,
+	&:hover {
+		color: #f6f5ef;
+		text-decoration: underline;
+	}
+`;
 
 export default NavComponents;
